8a78eaa0024eba13547b78f4ef445eb8630f8dbb
Skyler <50990788+TheSkyler-Dev@users.noreply.github.com>
2024-10-16 08:31:36 +0200

    Add latest codebase and implementations

---
+++ implementations/Mass.cpp
@@ -0,0 +1,70 @@
+#include "Mass.h"
+#include <iostream>
+
+Mass::Mass(double g, double kg, double t, double oz, double lb, double tn) : grams(g), kilos(kg), tonnes(t), ounces(oz), pounds(lb), tons(tn)
+{
+    //ctor
+    void validateInput();
+}
+
+//input validation
+void Mass::validateInput(){
+    if (grams < 0 || kilos < 0 || tonnes < 0 || ounces < 0 || pounds < 0 || tons < 0){
+        throw std::invalid_argument("Mass cannot be negative!");
+    }
+}
+
+//conversion logic
+void Mass::massConvert(){
+    std::cout << grams << " g is equivalent to " << grams / 1000 << " kg" << '\n';
+    std::cout << kilos << " kg is equivalent to " << kilos / 1000 << " t" << '\n';
+    std::cout << tonnes << " t is equivalent to " << tonnes * 1000 << " kg" << '\n';
+    std::cout << kilos << " kg is equivalent to " << kilos * 1000 << " g" << '\n';
+    std::cout << tonnes << " t is equivalent to " << tonnes * 1000000 << " g" << '\n';
+    std::cout << grams << " g is equivalent to " << grams / 1000000 << " t" << std::endl;
+    std::cout << ounces << " oz is equivalent to " << ounces / 16 << " lb" << '\n';
+    std::cout << pounds << " lb is equivalent to " << pounds / 2000 << " tn" << '\n';
+    std::cout << tons << " tn is equivalent to " << tons * 2000 << " lb" << '\n';
+    std::cout << pounds << " lb is equivalent to " << pounds * 16 << " oz" << '\n';
+    std::cout << ounces << " oz is equivalent to " << ounces / 32000 << " tn" << std::endl;
+    std::cout << grams << " g is equivalent to " << grams / 28.35 << " oz" << '\n';
+    std::cout << grams << " g is equivalent to " << grams / 453.6 << " oz" << '\n';
+    std::cout << grams << " g is equivalent to " << grams / 907200 << " tn" << '\n';
+    std::cout << kilos << " kg is equivalent to " << kilos / 35.274 << " oz" << '\n';
+    std::cout << kilos << " kg is equivalent to " << kilos * 2.205 << " lb" << '\n';
+    std::cout << kilos << " kg is equivalent to " << kilos / 907.2 << " tn" << '\n';
+    std::cout << tonnes << " t is equivalent to " << tonnes * 35270 << " oz" << '\n';
+    std::cout << tonnes << " t is equivalent to " << tonnes * 2205 << " lb" << '\n';
+    std::cout << tonnes << " t is equivalent to " << tonnes * 1.102 << " tn" << std::endl;
+}
+
+//setters
+void Mass::setGrams(double g){
+    grams = g;
+    validateInput();
+}
+
+void Mass::setKilos(double kg){
+    kilos = kg;
+    validateInput();
+}
+
+void Mass::setTonnes(double t){
+    tonnes = t;
+    validateInput();
+}
+
+void Mass::setOunces(double oz){
+    ounces = oz;
+    validateInput();
+}
+
+void Mass::setPounds(double lb){
+    pounds = lb;
+    validateInput();
+}
+
+void Mass::setTons(double tn){
+    tons = tn;
+    validateInput();
+}
